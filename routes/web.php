dec320fb832d0479308073bdb13580066469be20
Mouhamed-Talibi <mouha22talibi@gmail.com>
2025-06-05 23:22:23 +0100

    Setting up routes for login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AppController;
+    use App\Http\Controllers\AuthController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    // home page
+    Route::get('/', [AppController::class, 'index'])
+        ->name('home');
+
+    // authentification routes 
+    Route::prefix('auth')->name('auth.')->group( function () {
+        // login routes
+        Route::get('login', [AuthController::class, 'showLoginForm'])
+            ->name('loginForm');
+        Route::post('login', [AuthController::class, 'login'])
+            ->name('login');
+        // regsiter routes
+        Route::get('register', [AuthController::class, 'showRegistrationForm'])
+            ->name('regsitrationForm');
+        Route::post('login', [AuthController::class, 'register'])
+            ->name('regsiter');
+    });
